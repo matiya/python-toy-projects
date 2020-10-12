9254b53ead9d634e59374a6a037a2ad3d2d5c132
Matias <matias.siracusa@protonmail.com>
2020-10-12 19:06:56 -0300

    Mejorado el mensaje de error, si la página no se encuentra

--- simple-web-server/simple-web-server.py
+++ simple-web-server/simple-web-server.py
@@ -8,6 +8,7 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
     template = """\
 <html>
 <body>
+    <h2> {msg} </h2>
 <table>
 <tr>  <td>Header</td>         <td>Value</td>          </tr>
 <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
@@ -19,10 +20,18 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
 </body>
 </html>
 """
+    msg_ok = '<b style="color:green">Servidor funcionado </b>'
+    msg_error = '<b style="color:red"> Objeto no encontrado </b>'
 
     def do_GET(self):
         try:
             full_path = os.getcwd() + self.path
+
+            # pagina con estado ppal
+            if self.path == "/":
+                self.send_page()
+                return
+            # comprueba si existe el path
             if not os.path.exists(full_path) and not (
                 "favicon" or "icon.png" in full_path
             ):
@@ -39,33 +48,35 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         try:
             html_file = open(full_path, "r")
             content = html_file.read()
+            html_file.close()
         except Exception as msg:
             self.handle_error(msg)
         self.send_page(content.encode())
 
-    def create_page(self):
+    def create_page(self, status):
         values = {
             "date_time": self.date_time_string(),
             "client_host": self.client_address[0],
             "client_port": self.client_address[1],
             "command": self.command,
             "path": self.path,
+            "msg": (self.msg_ok if status == 200 else self.msg_error),
         }
         page = self.template.format(**values)
         print(page)
         return page
 
-    def send_page(self, content=None):
+    def send_page(self, content=None, status=200):
         if content is None:
-            content = self.create_page().encode()
-        self.send_response(200)
+            content = self.create_page(status).encode()
+        self.send_response(status)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
         self.wfile.write(content)
 
     def handle_error(self, msg):
         print(f">>> Error:  {msg}")
-        self.send_page()
+        self.send_page(None, 404)
 
 
 if __name__ == "__main__":
