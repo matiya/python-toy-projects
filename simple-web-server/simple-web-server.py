bc21a261893bfb3f03e026da572db17d9028b4cd
Matias <matias.siracusa@protonmail.com>
2020-10-19 08:50:58 -0300

    do_GET testea casos de manera mas robusta

--- simple-web-server/simple-web-server.py
+++ simple-web-server/simple-web-server.py
@@ -1,57 +1,94 @@
+#!/usr/bin/env python
+
 import os
 import http.server
+
+
+class case_directory:
+    def test(self, handler):
+        return os.path.isdir(handler.full_path)
+
+    def act(self, handler):
+        print("case_directory")
+        os.walk()  # regresa los archivos en un directorio
+        pass
+
+
+class case_file:
+    def test(self, handler):
+        return os.path.isfile(handler.full_path)
+
+    def act(self, handler):
+        print("case_existing_file")
+        handler.handle_file()
+
+
+class case_invalid_path:
+    def test(self, handler):
+        return not os.path.exists(handler.full_path)
+
+    def act(self, handler):
+        print("case_invalid_path")
+        handler.send_page(status=404)
+
+
+class case_error:
+    def test(self, handler):
+        return True
+
+    def act(self, handler):
+        print("case_error")
+        handler.handle_error(f"{handler.path} no encontrado")
 
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     """Regreso una página básica"""
 
     template = """\
-<html>
-<body>
-    <h2> {msg} </h2>
-<table>
-<tr>  <td>Header</td>         <td>Value</td>          </tr>
-<tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
-<tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
-<tr>  <td>Client port</td>    <td>{client_port}s</td> </tr>
-<tr>  <td>Command</td>        <td>{command}</td>      </tr>
-<tr>  <td>Path</td>           <td>{path}</td>         </tr>
-</table>
-</body>
-</html>
-"""
+               <html>
+               <body>
+                   <h2> {msg} </h2>
+               <table>
+               <tr>  <td>Header</td>         <td>Value</td>          </tr>
+               <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
+               <tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
+               <tr>  <td>Client port</td>    <td>{client_port}s</td> </tr>
+               <tr>  <td>Command</td>        <td>{command}</td>      </tr>
+               <tr>  <td>Path</td>           <td>{path}</td>         </tr>
+               </table>
+               </body>
+               </html>
+               """
     msg_ok = '<b style="color:green">Servidor funcionado </b>'
     msg_error = '<b style="color:red"> Objeto no encontrado </b>'
+    full_path = ""
+
+    Cases = [case_directory, case_file, case_invalid_path, case_error]
 
     def do_GET(self):
         try:
-            full_path = os.getcwd() + self.path
-
-            # pagina con estado ppal
-            if self.path == "/":
-                self.send_page()
-                return
-            # comprueba si existe el path
-            if not os.path.exists(full_path) and not (
-                "favicon" or "icon.png" in full_path
-            ):
-                raise Exception(f"{self.path} not found")
-            elif os.path.isfile(full_path):
-                self.handle_file(full_path)
-            else:
-                raise Exception(f"{self.path} is unknown object")
+            self.full_path = os.getcwd() + self.path
+            # iterar por cada caso, comprobar si aplica
+            # y  ejecutar una accion de ser así
+            print(f"{self.full_path=}")
+            for case in self.Cases:
+                # instancio la clase case
+                handler = case()
+                if handler.test(self):
+                    handler.act(self)
+                    break
 
         except Exception as msg:
             self.handle_error(msg)
 
-    def handle_file(self, full_path):
+    def handle_file(self):
         try:
-            html_file = open(full_path, "r")
+            html_file = open(self.full_path, "r")
             content = html_file.read()
             html_file.close()
+            self.send_page(content.encode())
         except Exception as msg:
             self.handle_error(msg)
-        self.send_page(content.encode())
 
     def create_page(self, status):
         values = {
